ca5008959d4110f81adab774dc834a92257e2681
Kamal Mango <kamal.mango7@gmail.com>
2022-01-17 16:57:23 +0100

    add coin list item

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ListItem from './components/ListItem';
+
+import { SAMPLE_DATA } from './assets/data/sampleData';
 
 export default function App() {
   return (
@@ -8,6 +11,14 @@ export default function App() {
         <Text style={styles.largeTitle}>Markets</Text>
       </View>
       <View style={styles.divider} />
+
+      <ListItem 
+        name={SAMPLE_DATA[0].name}
+        symbol={SAMPLE_DATA[0].symbol}
+        currentPrice={SAMPLE_DATA[0].current_price}
+        priceChangePercentage7d={SAMPLE_DATA[0].price_change_percentage_7d_in_currency}
+        logoUrl={SAMPLE_DATA[0].image}
+      />
     </View>
   );
 }
